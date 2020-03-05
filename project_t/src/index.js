cb63c8cbaebb553e34ef81cf517d5e1f227ae10d
Kerllsname <kirillpotapovnow@gmail.com>
2020-03-05 15:09:34 +0300

    Structure of App

---
+++ project_t/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './components/TodoList'
+import AppHeader from './components/AppHeader'
+import SearchPanel from './components/SearchPanel'
+
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
